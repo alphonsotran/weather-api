106808c2ebd2c748511abf2debb515c1e1d43264
alphonsotran <alphonsotran@gmail.com>
2018-02-06 21:40:38 +0100

    CREATE new folder and file called weather. MOVE weather api call to weather.js.

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-// const yargs = require('yargs');
-
-// const geocode = require('./geocode/geocode')
-
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: 'address',
-//             describe: 'Address to fetch weather for',
-//             string: true
-//         }
-// })
-//     .help()
-//     .alias('help', 'h')
-//     .argv;
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
-
-// 64f9aa89b94cdcdbb95f5601c0bfe9ca
-
-const request = require('request');
 const yargs = require('yargs');
 
-request({
-    url: `https://api.darksky.net/forecast/${process.env.DARK_SKY_APP}/52.5019332,13.4554471`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature)
-    } else {
-        console.log('Unable to fetch weather.')
-    }
+const geocode = require('./geocode/geocode')
+const weather = require('./weather/weather')
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
 })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage)
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+            }
+        });
+    }
+});
+
